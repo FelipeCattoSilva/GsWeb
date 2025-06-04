e84c0f84353d91006fd66962c6c2c1c8b973eabe
Raphael <raffa.aaron@gmail.com>
2025-06-04 17:34:29 -0300

    Adicionando funções pra trocar de tema

--- src/script/script.js
+++ src/script/script.js
@@ -21,4 +21,24 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         setTimeout(showSlides, 3000);
     }
+
+    // Troca de tema
+    const themeLight = document.getElementById('theme-light');
+    const themeDark = document.getElementById('theme-dark');
+    const themeGreen = document.getElementById('theme-green');
+
+    if (themeLight && themeDark && themeGreen) {
+        themeLight.onclick = () => {
+            document.body.classList.remove('theme-dark', 'theme-green');
+            document.body.classList.add('theme-light');
+        };
+        themeDark.onclick = () => {
+            document.body.classList.remove('theme-light', 'theme-green');
+            document.body.classList.add('theme-dark');
+        };
+        themeGreen.onclick = () => {
+            document.body.classList.remove('theme-light', 'theme-dark');
+            document.body.classList.add('theme-green');
+        };
+    }
 });
