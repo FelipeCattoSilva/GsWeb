d3c60c90d9229a5f40954ce26efbf822a942e2c4
Felipe <felipecattosilvabr@gmail.com>
2025-06-04 17:33:36 -0300

    Adicionando script slideshow

--- src/script/script.js
+++ src/script/script.js
@@ -5,4 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
     hamburgerBtn.addEventListener('click', function() {
         menu.classList.toggle('show');
     });
+
+    // SlideShow
+    let slideIndex = 0;
+    showSlides();
+    function showSlides() {
+        const slides = document.getElementsByClassName("mySlides");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        if (slides.length > 0) {
+            slides[slideIndex-1].style.display = "block";
+        }
+        setTimeout(showSlides, 3000);
+    }
 });
